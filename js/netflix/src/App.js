656baabf4cd01ae7b819b486fe8ded3250f8aa8c
Lorenzo Battilocchi <battill@amazon.com>
2021-06-05 20:49:27 +0100

    Continuing Jumbotron Creation
    Reached 50:13 in video, alignment not yet working

--- js/netflix/src/App.js
+++ js/netflix/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Jumbotron from './components/jumbotron';
+import jumboData from './fixtures/jumbo.json';
 
 export default function App() {
   return (
-    <p>Hello</p>
+    <Jumbotron.Container>
+      {jumboData.map((item ) =>(
+        <Jumbotron direction={item.direction} key={item.id}  >
+          <Jumbotron.Pane>
+            <Jumbotron.Title >{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle >{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Image src={item.image} alt={item.alt}/>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
+
+  
