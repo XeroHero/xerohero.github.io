32bfdad0017833c06805a70f4c0642b33c617027
Lorenzo Battilocchi <battill@amazon.com>
2021-06-20 15:09:01 +0100

    Add in Footer to Homepage
    Edited some elements sizes to make bigger text. Reduce as necessary in future

--- js/netflix/src/components/footer/index.js
+++ js/netflix/src/components/footer/index.js
@@ -27,5 +27,5 @@ Footer.Text = function FooterText({ children, ...restProps }) {
   return <Text {...restProps}>{children}</Text>;
 };
 Footer.Break = function FooterBreak({children, ...restProps}){
-  return <Break {...restProps}>{children}</Break>;
+  return <break {...restProps}>{children}</break>;
 }
--- js/netflix/src/components/footer/styles/footer.js
+++ js/netflix/src/components/footer/styles/footer.js
@@ -20,7 +20,7 @@ export const Column = styled.div`
 
 export const Row = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
   grid-gap: 15px;
 
   @media (max-width: 1000px) {
@@ -31,18 +31,18 @@ export const Row = styled.div`
 export const Link = styled.a`
   color: #757575;
   margin-bottom: 20px;
-  font-size: 13px;
+  font-size: 20px;
   text-decoration: none;
 `;
 
 export const Title = styled.p`
-  font-size: 16px;
+  font-size: 23px;
   color: #757575;
   margin-bottom: 40px;
 `;
 
 export const Text = styled.p`
-  font-size: 13px;
+  font-size: 18px;
   color: #757575;
   margin-bottom: 40px;
 `;
