0e17472828d50435c1536dd0dcf14550c1acaede
Lorenzo Battilocchi <battill@amazon.com>
2021-07-31 17:24:58 +0100

    Start Domain Caching process

---
+++ js/rps/app.js
@@ -0,0 +1,16 @@
+// console.log('hello');
+
+const userScore = 0;
+const cpuScore =0;
+
+const userScore_span = document.getElementById('user-score');
+const cpuScore_span = document.getElementById('cpu-score');
+const scoreboard_div = document.querySelector('.scoreboard');
+const result_div = document.querySelector('.result';)
+
+const rock_div = document.getElementById('rock-choice');
+const paper_div = document.getElementById('paper-choice');
+const scissor_div = document.getElementById('scissor-choice');
+
+
+rock_div.addEventListener('')
